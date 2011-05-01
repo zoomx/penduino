16cc9a6d9d6fc52bf65cc63acf7b08f4df08d850
gee7@aber.ac.uk <gee7@aber.ac.uk@b338f817-14de-e497-796f-c3291ae45fb1>
2011-05-01 00:15:04 +0000

    Updated core module design

---
+++ trunk/JavaControlGui/src/core/src/main/java/com/googlecode/penduino/core/model/ConnectionInUseException.java
@@ -0,0 +1,5 @@
+package com.googlecode.penduino.core.model;
+
+@SuppressWarnings("serial")
+public class ConnectionInUseException extends Exception {
+}
--- trunk/JavaControlGui/src/core/src/main/java/com/googlecode/penduino/core/model/Robot.java
+++ trunk/JavaControlGui/src/core/src/main/java/com/googlecode/penduino/core/model/Robot.java
@@ -2,26 +2,36 @@ package com.googlecode.penduino.core.model;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+import com.googlecode.penduino.core.ModelUpdater;
+import com.googlecode.penduino.core.PidSerialRequester;
 import com.googlecode.penduino.core.events.PidGainChangedEvent;
 import com.googlecode.penduino.core.events.PitchChangedEvent;
 import com.googlecode.penduino.core.interfaces.PidGainController;
 import com.googlecode.penduino.core.interfaces.PidTuningListener;
 import com.googlecode.penduino.core.interfaces.PitchChangeListener;
+import com.googlecode.penduino.serial.ArduinoSerialConnectionFactory;
+import com.googlecode.penduino.serial.exceptions.PortInUseException;
 
 public enum Robot {
 
 	INSTANCE;
 	
+	private Robot(){
+		PidSerialRequester req = new PidSerialRequester();
+		gainController = req;
+		ArduinoSerialConnectionFactory.INSTANCE.registerMessageCreator(req);
+		ArduinoSerialConnectionFactory.INSTANCE.registerMessageHandler(new ModelUpdater());	
+	}
 	
-	private Collection<PidGainController> gainControllers = new ArrayList<PidGainController>();
+	
+	private PidGainController gainController;
 	private Collection<PitchChangeListener> pitchListeners = new ArrayList<PitchChangeListener>();
 	private Collection<PidTuningListener> pidListeners = new ArrayList<PidTuningListener>();
 	
 	public void requestPidGainChange(PidGainVariable pgv, PidChangeValue pcv){
-		for(PidGainController controller : gainControllers){
-			controller.changePidGain(pgv, pcv);
-		}
+			gainController.changePidGain(pgv, pcv);
 	}
 	
 	public void setPitch(PitchChangedEvent pitch){
@@ -36,15 +46,27 @@ public enum Robot {
 		}
 	}
 	
-	public void registerPidGainController(PidGainController controller){
-		gainControllers.add(controller);
-	}
-	
 	public void registerPitchChangeListener(PitchChangeListener listener){
 		pitchListeners.add(listener);
 	}
 
 	public void registerPidTuningListener(PidTuningListener listener){
 		pidListeners.add(listener);
+	}
+	
+	public static List<String> getAvailibleConnectionsList(){
+		return ArduinoSerialConnectionFactory.getPortNamesAvailable();
+	}
+	
+	public void startRobotConnection(String connectionName) throws ConnectionInUseException{
+		try {
+			ArduinoSerialConnectionFactory.INSTANCE.openDefaultArduinoSerialConnection(connectionName);
+		} catch (PortInUseException e) {
+			throw new ConnectionInUseException();
+		}
+	}
+	
+	public void closeRobotConnection(){
+		ArduinoSerialConnectionFactory.INSTANCE.close();
 	}
 }
