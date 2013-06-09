e856ad67fc6ec6ca71827697c47694c4ed08eac6
gavin.esberger@gmail.com <gavin.esberger@gmail.com@b338f817-14de-e497-796f-c3291ae45fb1>
2013-06-09 21:34:24 +0000

    Changed to use Executors for jobs. Changed structure. Improved builder. Added ErrorHandler

--- trunk/JavaControlGui/src/serial/src/main/java/com/googlecode/penduino/serial/ArduinoSerialConnection.java
+++ trunk/JavaControlGui/src/serial/src/main/java/com/googlecode/penduino/serial/ArduinoSerialConnection.java
@@ -1,49 +1,60 @@
 package com.googlecode.penduino.serial;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.TooManyListenersException;
-
 import gnu.io.CommPortIdentifier;
-import gnu.io.NoSuchPortException;
 import gnu.io.PortInUseException;
 import gnu.io.SerialPort;
 import gnu.io.SerialPortEvent;
 import gnu.io.SerialPortEventListener;
 import gnu.io.UnsupportedCommOperationException;
 
-/*
- * Default constructor on purpose
- */
-class ArduinoSerialConnection implements SerialPortEventListener { 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.TooManyListenersException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import com.googlecode.penduino.serial.internal.ErrOutErrorHandler;
+import com.googlecode.penduino.serial.internal.HandleErrorJob;
+import com.googlecode.penduino.serial.internal.NotifyListenerJob;
+
+public class ArduinoSerialConnection implements SerialPortEventListener {
+
+	private static final String CHARSET_NAME = "ASCII";
+	private static final int TIME_OUT = 2000;
+	private static final int BACKGROUND_THREAD_COUNT = 8;
 
 	private final SerialPort serialPort;
-	private static final int TIME_OUT = 2000;
 	private final BufferedInputStream input;
 	private final OutputStream output;
-	private final byte[] bufferedBytes;
-	private int totalBytesLength = 0;
-	
-	private final byte terminateByte;
 
-	private Collection<ArduinoSerialMessageHandler> messageHandlers = new ArrayList<ArduinoSerialMessageHandler>();
-	
-	public ArduinoSerialConnection(String portName, int bufferSize, int dataRate, byte terminateByte)
-			throws NoSuchPortException, PortInUseException,
-			UnsupportedCommOperationException, IOException,
-			TooManyListenersException {
-		
+	private final byte[] bufferedBytes;
+	private final byte terminateByte;
+	private int totalBytesLength = 0;
+
+	private final Collection<ArduinoSerialMessageListener> messageListeners = new ArrayList<ArduinoSerialMessageListener>();
+	private final ArduinoSerialErrorHandler errorHandler;
+
+	private final Lock serialReadLock = new ReentrantLock();
+
+	private final ExecutorService executor = Executors
+			.newFixedThreadPool(BACKGROUND_THREAD_COUNT);
+
+	private ArduinoSerialConnection(final CommPortIdentifier commPort,
+			final int bufferSize, final int dataRate, final byte terminateByte,
+			final ArduinoSerialErrorHandler errorHandler)
+			throws PortInUseException, UnsupportedCommOperationException,
+			IOException, TooManyListenersException {
+		this.errorHandler = errorHandler;
 		this.terminateByte = terminateByte;
-		
-		CommPortIdentifier portId = CommPortIdentifier
-				.getPortIdentifier(portName);
-		serialPort = (SerialPort) portId.open(this.getClass().getName(),
+
+		serialPort = (SerialPort) commPort.open(this.getClass().getName(),
 				TIME_OUT);
 
 		serialPort.setSerialPortParams(dataRate, SerialPort.DATABITS_8,
@@ -59,64 +70,134 @@ class ArduinoSerialConnection implements SerialPortEventListener {
 	}
 
 	@Override
-	public synchronized void serialEvent(SerialPortEvent event) {
-		if (event.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
-			try {
-				for (int i = 0; i< input.available(); i++) {
-					int b = input.read();
-					
-					if (b != terminateByte) {
-						this.bufferedBytes[totalBytesLength++]=(byte) b;
-					} else{
-						
-						this.dealWithAFullMessage(new String(bufferedBytes, 0, totalBytesLength, "ASCII"));
-						totalBytesLength = 0;
-					}
-				}
+	public void serialEvent(final SerialPortEvent event) {
+		this.serialReadLock.lock();
+		try {
+			attemptProcessSerialEvent(event);
+		} catch (final IOException e) {
+			handleSerialProcessingError(e);
+		} finally {
+			this.serialReadLock.unlock();
+		}
+	}
 
-			} catch (Exception e) {
-				System.err.println(e.toString());
+	private void attemptProcessSerialEvent(final SerialPortEvent event)
+			throws IOException {
+		if (event.getEventType() == SerialPortEvent.DATA_AVAILABLE) {
+			for (int i = 0; i < input.available(); i++) {
+				int b;
+				b = input.read();
+				if (b != terminateByte) {
+					this.bufferedBytes[totalBytesLength++] = (byte) b;
+				} else {
+					this.notifyListeners(new String(bufferedBytes, 0,
+							totalBytesLength, CHARSET_NAME));
+					totalBytesLength = 0;
+				}
 			}
 		}
-		
 	}
 
-	public synchronized void close() {
-		if (serialPort != null) {
-			serialPort.removeEventListener();
-			serialPort.close();
+	/*
+	 * TODO how would create a handler to stop processing message and would that
+	 * work if it error-ed during a shut down. Might be better to move back the
+	 * previous by passing in the error handler once the connection has been
+	 * create. Still nto entirely sure about it though
+	 */
+	private void handleSerialProcessingError(final Exception e) {
+		executor.execute(new HandleErrorJob(errorHandler, e));
+	}
+
+	private void notifyListeners(final String msg) {
+		for (final ArduinoSerialMessageListener listener : messageListeners) {
+			executor.execute(new NotifyListenerJob(listener, msg));
 		}
 	}
 
-	private void dealWithAFullMessage(String msg) {
-		for (ArduinoSerialMessageHandler msgListener : messageHandlers) {
-			msgListener.arduinoSerialMessageEvent(msg);
-		}
+	public void addSerialMessageListener(
+			final ArduinoSerialMessageListener listener) {
+		this.messageListeners.add(listener);
 	}
 
-	public void addSerialMessageHandler(ArduinoSerialMessageHandler asml) {
-		this.messageHandlers.add(asml);
+	public void sendOverSerial(final String msg) throws IOException {
+		output.write(msg.getBytes(CHARSET_NAME));
 	}
-	
-	public static List<String> getPortNamesAvailable(){
-		@SuppressWarnings("rawtypes")
-		Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
-		List<String> portNames = new ArrayList<String>();
-		while(portEnum.hasMoreElements()){
-			CommPortIdentifier currPortId = (CommPortIdentifier) portEnum.nextElement();
-			portNames.add(currPortId.getName());
-		}
-		return portNames;
-	}
-	
-	public void sendOverSerial(String msg){
+
+	/*
+	 * From the Arduino playground
+	 * 
+	 * The serial port's close method should be called from a method that is
+	 * synchronized with the serialEvent method, which will prevent any
+	 * exceptions from appearing during program shutdown.
+	 */
+	public void close() {
+		this.serialReadLock.lock();
 		try {
-			output.write(msg.getBytes("ASCII"));
-		} catch (UnsupportedEncodingException e) {
-			System.err.println(e.toString());
-		} catch (IOException e) {
-			System.err.println(e.toString());
+			if (serialPort != null) {
+				serialPort.removeEventListener();
+			}
+			if (input != null) {
+				input.close();
+			}
+		} catch (final IOException e) {
+			handleSerialProcessingError(e);
+		} finally {
+			this.serialReadLock.unlock();
 		}
+		executor.shutdownNow();
 	}
 
+	public static List<CommPortIdentifier> getPortNamesAvailable() {
+		final Enumeration<?> portEnum = CommPortIdentifier.getPortIdentifiers();
+		final List<CommPortIdentifier> ports = new ArrayList<CommPortIdentifier>();
+		while (portEnum.hasMoreElements()) {
+			ports.add((CommPortIdentifier) portEnum.nextElement());
+		}
+		return ports;
+	}
+
+	public class Builder {
+		private final CommPortIdentifier commPort;
+		private int bufferSize = 150;
+		private int baudRate = 9600;
+		private byte terminatingByte = (byte) 10;
+		private ArduinoSerialErrorHandler errorHandler = new ErrOutErrorHandler();
+
+		public ArduinoSerialConnection build() throws PortInUseException,
+				UnsupportedCommOperationException, IOException,
+				TooManyListenersException {
+			return new ArduinoSerialConnection(commPort, bufferSize, baudRate,
+					terminatingByte, errorHandler);
+		}
+
+		public Builder(final CommPortIdentifier commPort) {
+			this.commPort = commPort;
+		}
+
+		public Builder baudRate(final int baudRate) {
+			this.baudRate = baudRate;
+			return this;
+		}
+
+		public Builder baudRate(final BaudRate baudRate) {
+			this.baudRate = baudRate.getIntValue();
+			return this;
+		}
+
+		public Builder bufferSize(final int bufferSize) {
+			this.bufferSize = bufferSize;
+			return this;
+		}
+
+		public Builder terminatingByte(final byte terminatingByte) {
+			this.terminatingByte = terminatingByte;
+			return this;
+		}
+
+		public Builder errorHandler(final ArduinoSerialErrorHandler errorHandler) {
+			this.errorHandler = errorHandler;
+			return this;
+		}
+
+	}
 }
